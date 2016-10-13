59598d0418f90ba592f03bf2b219a317e6ceb6dd
wangguangjun <550234256@qq.com>
2016-10-13 14:52:49 +0800

    11

--- quanxian-web/src/main/java/com/wang/StartSpring.java
+++ quanxian-web/src/main/java/com/wang/StartSpring.java
@@ -1,21 +1,9 @@
 package com.wang;
 
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-
 /**
- * Created by Administrator on 2016/10/10.
+ * Created by Administrator on 2016/10/13.
  */
 @SpringBootApplication
-@ComponentScan({"com.wang.services","com.wang.controller","com.fc"})
-@MapperScan("com.wang.mapper")
-public class StartSpring extends SpringBootServletInitializer {
-    public static void main(String[] args){
-        SpringApplication.run(StartSpring.class, args);
-        System.out.println("启动了");
-    }
+public class StartSpring {
 
 }
